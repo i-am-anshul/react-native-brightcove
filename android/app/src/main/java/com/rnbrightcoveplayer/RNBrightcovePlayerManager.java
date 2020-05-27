dac78b18b7e7d16b9f08b704651bae4306e77f0a
Anshul Sharma <anshul@particle41.com>
2020-05-27 18:25:32 +0530

    Android side setup for Brightcove Integration - Setting up gradle files - setup brightcove package - setup brightcove manager - setup brightcove view

---
+++ android/app/src/main/java/com/rnbrightcoveplayer/RNBrightcovePlayerManager.java
@@ -0,0 +1,54 @@
+package com.rnbrightcoveplayer;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+
+public class RNBrightcovePlayerManager extends ViewGroupManager<RNBrightcovePlayerView> {
+
+    public static final String REACT_CLASS = "RNBrightcovePlayer";
+
+    private ThemedReactContext themedReactContext;
+    private static ReactApplicationContext applicationContext;
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    public RNBrightcovePlayerManager(ReactApplicationContext reactApplicationContext) {
+        super();
+        this.applicationContext = reactApplicationContext;
+    }
+
+    public RNBrightcovePlayerView createViewInstance(ThemedReactContext themedReactContext) {
+        this.themedReactContext = themedReactContext;
+        RNBrightcovePlayerView brightCovePlayerView = RNBrightcovePlayerView.getInstance(themedReactContext);
+        return brightCovePlayerView;
+    }
+
+
+    @ReactProp(name = "policyKey")
+    public void setPolicyKey(RNBrightcovePlayerView view, String policyKey) {
+
+        view = RNBrightcovePlayerView.getInstance(themedReactContext);
+        view.setPolicyKey(policyKey);
+    }
+
+    @ReactProp(name = "accountId")
+    public void setAccountId(RNBrightcovePlayerView view, String accountId) {
+
+        view = RNBrightcovePlayerView.getInstance(themedReactContext);
+        view.setAccountId(accountId);
+    }
+
+    @ReactProp(name = "videoId")
+    public void setVideoId(RNBrightcovePlayerView view, String videoId) {
+        view = RNBrightcovePlayerView.getInstance(themedReactContext);
+        view.setVideoId(videoId);
+    }
+
+
+}
