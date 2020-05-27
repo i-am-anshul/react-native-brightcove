aafb7d271b9a54ae3a3ece33fff3936d57d2296d
Anurag Dwivedi <anurag@particle41.com>
2020-05-27 19:26:21 +0530

    Add react-native brightcove player component

---
+++ src/native-modules/RNBrightCovePlayer.tsx
@@ -0,0 +1,7 @@
+import {NativeModules, requireNativeComponent} from 'react-native';
+export const {RNBrightcovePlayer} = NativeModules;
+// module.exports= RNBrightcovePlayer;
+// const NativeBrightcovePlayer = requireNativeComponent(
+//   'RNBrightcovePlayer',
+//   RNBrightcovePlayer,
+// );
---
+++ src/util/constants.ts
@@ -0,0 +1,11 @@
+export type stringConstantsType = {
+  BCOV_POLICY_KEY: string;
+  ACCOUNT_ID: string;
+  VIDEO_ID: string;
+};
+export const stringConstants: stringConstantsType = {
+  BCOV_POLICY_KEY:
+    'BCpkADawqM3n0ImwKortQqSZCgJMcyVbb8lJVwt0z16UD0a_h8MpEYcHyKbM8CGOPxBRp0nfSVdfokXBrUu3Sso7Nujv3dnLo0JxC_lNXCl88O7NJ0PR0z2AprnJ_Lwnq7nTcy1GBUrQPr5e',
+  ACCOUNT_ID: '4800266849001',
+  VIDEO_ID: '5754208017001',
+};
