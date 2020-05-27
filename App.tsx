6f205573a0d8d493dc77fa001a4091452c6120d0
Anurag Dwivedi <anurag@particle41.com>
2020-05-28 00:45:58 +0530

    Update CustomVideoPlayer to add style to brightCove Player

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
-import {CustomVideoPlayer} from './src/components/video-player/CustomVideoPlayer';
+import {
+  CustomVideoPlayer,
+  styles,
+} from './src/components/video-player/CustomVideoPlayer';
 
-declare const global: {HermesInternal: null | {}};
-
+const {viewStyle} = styles;
 const App = () => {
   return (
-    <View style={{flex: 1}}>
+    <View style={viewStyle}>
       <CustomVideoPlayer />
     </View>
   );
--- src/components/video-player/CustomVideoPlayer.tsx
+++ src/components/video-player/CustomVideoPlayer.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import {RNBrightcovePlayer} from '../../native-modules/RNBrightCovePlayer';
 import {stringConstants} from '../../util/constants';
-import { requireNativeComponent,View } from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {RNBCPlayer} from '../common/RNBCPlayer';
 
-export const CustomVideoPlayer = (props) => {
+export const styles = StyleSheet.create({
+  viewStyle: {
+    flex: 1,
+  },
+  brightcovePlayerStyle: {
+    width: '100%',
+    height: 200,
+  },
+});
+
+export const CustomVideoPlayer = () => {
   const {BCOV_POLICY_KEY, ACCOUNT_ID, VIDEO_ID} = stringConstants;
+  const {viewStyle} = styles;
 
   return (
-    <View style={{flex: 1, backgroundColor: 'pink'}}>
-      <RNBrightcovePlayer
-      style={{width: 600, height: 200}}
-      {...props}
+    <View style={viewStyle}>
+      <RNBCPlayer
         policyKey={BCOV_POLICY_KEY}
         accountId={ACCOUNT_ID}
         videoId={VIDEO_ID}
